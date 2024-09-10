35eeb3df8798800bb57dc9daa3dbebf5730f4c3b
trymenghuy <menghuy157@gmail.com>
2024-09-10 14:23:29 +0700

    update flutter package

--- flutter-webrtc-app/lib/main.dart
+++ flutter-webrtc-app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+
 import 'screens/join_screen.dart';
 import 'services/signalling.service.dart';
 
@@ -13,7 +14,7 @@ class VideoCallApp extends StatelessWidget {
   VideoCallApp({super.key});
 
   // signalling server url
-  final String websocketUrl = "http://192.168.0.126:5555/";
+  final String websocketUrl = "http://192.168.0.121:5555/";
 
   // generate callerID of local user
   final String selfCallerID =
@@ -30,7 +31,6 @@ class VideoCallApp extends StatelessWidget {
     // return material app
     return MaterialApp(
       darkTheme: ThemeData.dark().copyWith(
-        useMaterial3: true,
         colorScheme: const ColorScheme.dark(),
       ),
       themeMode: ThemeMode.dark,
